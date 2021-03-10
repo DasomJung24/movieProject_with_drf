79cfe14f308d2613b0c3359a541ff7f882d7d164
DasomJung24 <jeongdasom6@gmail.com>
2021-03-10 17:35:52 +0900

    reservation admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Reservation, ReservationItem, Theater, City, TheaterScreen, Screen, TicketType
 
-# Register your models here.
+admin.site.register(Reservation)
+admin.site.register(ReservationItem)
+admin.site.register(Theater)
+admin.site.register(City)
+admin.site.register(TheaterScreen)
+admin.site.register(Screen)
+admin.site.register(TicketType)
