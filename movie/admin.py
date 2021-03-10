098fe6e1c9ed8e016899327a7ad8a36ba00a4154
DasomJung24 <jeongdasom6@gmail.com>
2021-03-10 17:32:44 +0900

    movie admin

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie, Image, Rating, AudienceRating, Tag, Actor, Director, Genre, Type
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Image)
+admin.site.register(Rating)
+admin.site.register(AudienceRating)
+admin.site.register(Tag)
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Genre)
+admin.site.register(Type)
