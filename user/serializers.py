6359a9f0896c7713c91f9e2ca68f336f7d6a3b6d
DasomJung24 <jeongdasom6@gmail.com>
2021-03-12 00:09:34 +0900

    validate 수정

--- user/serializers.py
+++ user/serializers.py
@@ -7,10 +7,14 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
-        if not re.match(r'^.*(?=^.{8,12}$)(?=.*\d)(?=.*[a-zA-Z]).*$', attrs['account']):
-            raise serializers.ValidationError('account is not available')
-        elif not re.match(r'^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$', attrs['password']):
-            raise serializers.ValidationError('password is not available')
+        if attrs['is_user']:
+            if not attrs['account'] or not attrs['email']:
+                raise serializers.ValidationError('account와 email을 입력해주세요.')
+            elif not re.match(r'^.*(?=^.{8,12}$)(?=.*\d)(?=.*[a-zA-Z]).*$', attrs['account']):
+                raise serializers.ValidationError('account 형식을 지켜주세요.')
+
+        if not re.match(r'^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$', attrs['password']):
+            raise serializers.ValidationError('password 형식을 지켜주세요.')
         else:
             return attrs
 
