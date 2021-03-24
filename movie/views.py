6edaab6aab23dffa9b8c98e9d9be8278e36e96de
DasomJung24 <jeongdasom6@gmail.com>
2021-03-25 00:25:39 +0900

    url 수정 및 movie list/detail refactoring

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
@@ -8,18 +8,19 @@ from .models import *
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
+    permission_classes = [permissions.AllowAny]
 
-    def list(self, request):
-        movie = Movie.objects.all()
-        serializer = MovieSerializer(movie, many=True)
-        return Response({'result': serializer.data})
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        t = self.request.query_params.get('type', None)
+        if t:
+            queryset = queryset.filter(type=t)
+        return queryset
 
 
 class MovieDetailViewSet(viewsets.ModelViewSet):
     serializer_class = MovieDetailSerializer
-
-    def retrieve(self, request, *args, **kwargs):
-        queryset = Movie.objects.all()
-        movie = get_object_or_404(queryset, pk=kwargs['movie_id'])
-        serializer = MovieDetailSerializer(movie)
-        return Response({'result': serializer.data})
+    permission_classes = [permissions.AllowAny]
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'movie_id'
+    queryset = Movie.objects.all()
