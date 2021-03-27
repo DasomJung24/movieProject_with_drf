5b4dc5a8b074ee29f9c14dd15317031290a09d6c
DasomJung24 <jeongdasom6@gmail.com>
2021-03-28 00:19:20 +0900

    movie detail serializer refactoring

--- movie/serializers.py
+++ movie/serializers.py
@@ -26,85 +26,42 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'audience_rating', 'opening_date', 'images']
 
 
-class ActorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Actor
-        fields = ('name', )
-
-
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Genre
-        fields = ('name', )
-
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ('name', )
-
-
-class TypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Type
-        fields = ('name', )
-
-
-class AudienceRatingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AudienceRating
-        fields = ('grade', )
-
-
-class DirectorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Director
-        fields = ('name', )
-
-
 class MovieDetailSerializer(serializers.ModelSerializer):
-    actor = serializers.SerializerMethodField('get_actors')
-    genre = serializers.SerializerMethodField('get_genres')
-    tag = serializers.SerializerMethodField('get_tags')
-    type = serializers.SerializerMethodField('get_type')
-    audience_rating = serializers.SerializerMethodField('get_audience_rating')
-    director = serializers.SerializerMethodField('get_directors')
-    images = serializers.SerializerMethodField('get_images')
-
-    def get_images(self, movie):
-        image = Image.objects.filter(movie=movie)
-        serializers = ImageSerializer(image, many=True)
-        return serializers.data
-
-    def get_actors(self, movie):
-        actors = Actor.objects.filter(movie=movie)
-        serializers = ActorSerializer(actors, many=True)
-        return serializers.data
-
-    def get_genres(self, movie):
-        genres = Genre.objects.filter(movie=movie)
-        serializers = GenreSerializer(genres, many=True)
-        return serializers.data
-
-    def get_tags(self, movie):
-        tags = Tag.objects.filter(movie=movie)
-        serializers = TagSerializer(tags, many=True)
-        return serializers.data
-
-    def get_type(self, movie):
-        types = Type.objects.filter(movie=movie)
-        serializers = TypeSerializer(types, many=True)
-        return serializers.data
-
-    def get_audience_rating(self, movie):
-        audience_rating = AudienceRating.objects.get(movie=movie)
-        serializers = AudienceRatingSerializer(audience_rating, many=False)
-        return serializers.data
-
-    def get_directors(self, movie):
-        directors = Director.objects.filter(movie=movie)
-        serializers = DirectorSerializer(directors, many=True)
-        return serializers.data
+    actor = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    genre = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    tag = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='name'
+    )
+    type = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    audience_rating = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='grade'
+    )
+    director = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    images = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='url'
+    )
 
     class Meta:
         model = Movie
