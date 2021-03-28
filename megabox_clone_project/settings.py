79dfef0d0df22d74064ed37a40a35353ba404db7
DasomJung24 <jeongdasom6@gmail.com>
2021-03-29 04:37:50 +0900

    문서화 추가

--- megabox_clone_project/settings.py
+++ megabox_clone_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth',
     'rest_framework_jwt',
+    'drf_yasg',
     'user',
     'movie',
     'reservation',
--- megabox_clone_project/urls.py
+++ megabox_clone_project/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from rest_framework.permissions import AllowAny
 from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token, obtain_jwt_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Megabox Clone API',
+        default_version='v1',
+        description='Megabox clone project 문서입니다.',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jeongdasom6@gmail.com"),
+        license=openapi.License(name="Jay's CodeFactory"),
+    ),
+    validators=['flex'], #'ssv'],
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +28,7 @@ urlpatterns = [
     path('jwt-auth/refresh/', refresh_jwt_token),  # JWT 토큰 갱신
     path('jwt-auth/verify/', verify_jwt_token),   # JWT 토큰 확인
     path('reservations', include('reservation.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)/v1$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/v1/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/v1/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
