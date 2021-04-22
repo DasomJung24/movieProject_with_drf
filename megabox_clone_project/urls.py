d9e25cb953748a05248dc5a645dbc1f5a0b31c71
DasomJung24 <jeongdasom6@gmail.com>
2021-04-22 23:29:09 +0900

    user 수정

--- megabox_clone_project/urls.py
+++ megabox_clone_project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movie.urls')),
     path('users/', include('user.urls')),
-    path('jwt-auth/', obtain_jwt_token),  # 로그인 & JWT 토큰 획득
+    # path('jwt-auth/', obtain_jwt_token),  # 로그인 & JWT 토큰 획득
     path('jwt-auth/refresh/', refresh_jwt_token),  # JWT 토큰 갱신
     path('jwt-auth/verify/', verify_jwt_token),   # JWT 토큰 확인
     path('reservations', include('reservation.urls')),
