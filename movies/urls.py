51bb0518a4de67da8d634f2108c0c05a6093b441
DasomJung24 <jeongdasom6@gmail.com>
2021-06-10 17:24:42 +0900

    url update

--- movies/urls.py
+++ movies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MovieListView.as_view(), name='movie_list'),
-    path('<int:movie_id>', views.MovieDetailView.as_view(), name='movie_detail'),
-    path('<int:movie_id>/likes', views.LikeView.as_view(), name='like_create'),
+    path('movies', views.MovieListView.as_view(), name='movie_list'),
+    path('movies/<int:movie_id>', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('movies/<int:movie_id>/likes', views.LikeView.as_view(), name='like_create'),
 ]
